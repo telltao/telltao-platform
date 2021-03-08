2a3aa81782ae42b6a4b087395783ebdf3d3dff2e
telltao <telltao@qq.com>
2021-03-08 22:47:41 +0800

    学习备份

--- telltao-dubbo-server/src/test/java/cn/telltao/dubbo/server/controller/Test.java
+++ telltao-dubbo-server/src/test/java/cn/telltao/dubbo/server/controller/Test.java
@@ -26,69 +26,56 @@ public class Test {
         if (numbers.length <=0) {
             new IllegalArgumentException("参数不可为空!");
         }
-        //标记此数是否包含负数 默认不包含
-        //boolean minusFlag = false;
-        //标记是否包含正整数 默认不包含
-        //boolean positiveFlag = false;
+
         TreeMap<Integer,Integer> treeMap = new TreeMap();
         for (int i = 0; i < numbers.length; i++) {
             if (treeMap.containsKey(numNumber)) {
                 //找出给定的数字的重复数字
                 return (int)treeMap.get(numNumber);
             }
-            /*if (!minusFlag && numbers[i] < 0) {
-                //有负数
-                minusFlag = true;
-            } else if (minusFlag && numbers[i] > 0) {
-                //有负数也有正数
-                positiveFlag = true;
-                minusFlag = true;
-            }*/
-            treeMap.put(numbers[i],numbers[i]);
+            treeMap.put(i,numbers[i]);
         }
         //判断数字是否超过 N-1
         if (treeMap.containsKey(numbers.length-1)) {
             Object num = treeMap.get(numbers.length - 1);
-            return (int)num;
-        }
-        //利用treeMap的有序和自动排序机制
-        //如果有负数
-        //有负数取第一个 没有负数取第二个
-
-        Integer firstKey = treeMap.firstKey();
-        Integer lastKey = treeMap.lastKey();
-        if (firstKey < 0 && lastKey > 0) {
-           return firstKey;
-        } else if (firstKey < 0 && lastKey < 0) {
-            return lastKey;
+            return (int) num;
         }
         return treeMap.firstKey();
-        /*if (minusFlag && !positiveFlag) {
-            return treeMap.lastKey();
-        } else if (positiveFlag && !minusFlag) {
-            //表示没有负数 返回第一个
-            return treeMap.lastKey();
-        }
-        return treeMap.firstKey();*/
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
 
         int [] num  = new int[] {1,2,3,4,5};
-        //Assert.assertEquals(1,findStrangeNum(num, 1));
+        Assert.assertEquals(1,findStrangeNum(num, 1));
         System.out.println("返回数字为1,验证通过");
 
         int [] num2  = new int[] {1,3,2,4};
-        //Assert.assertEquals(3,findStrangeNum(num2, 8));
+        Assert.assertEquals(3,findStrangeNum(num2, 8));
         System.out.println("返回数字为3,验证通过");
 
         int [] num3  = new int[] {1,3,2,4,-1,7};
         Assert.assertEquals(-1,findStrangeNum(num3, 8));
         System.out.println("返回数字为-1,验证通过");
 
-        int [] num4  = new int[] {-1,-3,-2,-4,-6,7};
-        Assert.assertEquals(7,findStrangeNum(num4, 8));
-        System.out.println("返回数字为7,验证通过");
+        int [] num4  = new int[] {-1,-3,-2,-4,-1,7};
+        Assert.assertEquals(-1,findStrangeNum(num4, 8));
+        System.out.println("返回数字为-1,验证通过");
+
     }
 
 }
---
+++ telltao-thread-model/src/main/java/cn/telltao/collections/TestArrayList.java
@@ -0,0 +1,21 @@
+package cn.telltao.collections;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author telltao
+ * @Date 2021/1/26 17:31
+ */
+public class TestArrayList {
+
+    public static void main(String[] args) {
+
+        List list = new ArrayList();
+
+        Map map = new HashMap();
+
+    }
+}
